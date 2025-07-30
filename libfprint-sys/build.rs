3025d6db79ab8041b8317dac083deed1521ccf21
Narthana Epa <narthana.epa@gmail.com>
2025-07-30 21:16:19 +1000

    Update bingen and rust edition

--- libfprint-sys/build.rs
+++ libfprint-sys/build.rs
@@ -21,7 +21,7 @@ fn main() {
                 .map(|path| format!("-I{}", path.to_string_lossy())),
         )
         .header("wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
